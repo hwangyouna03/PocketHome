3b96e826434c8d58f19b69fe756ed16183b000cc
hwangyouna03 <hwangyouna03@daum.net>
2021-04-25 21:43:41 +0900

    layout

---
+++ app/src/main/java/kr/hs/emirim/s2019s40/pockethome1/MainActivity.java
@@ -0,0 +1,64 @@
+package kr.hs.emirim.s2019s40.pockethome1;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+    private String htmlUrl = "https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=101&sid2=260";
+    private TextView textView;
+    private String htmlContentStringFormat="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =(TextView)findViewById(R.id.textView);
+        textView.setMovementMethod(new ScrollingMovementMethod()); //스크롤 가능
+
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
+    }
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try{
+                Document doc = (Document) Jsoup.connect(htmlUrl).get();
+
+                Elements titles = doc.select("div.list_body newsflash_body");
+                for(Element e: titles){
+                    System.out.println("title" + e.text());
+                    htmlContentStringFormat += e.text().trim() +"\n";
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textView.setText(htmlContentStringFormat);
+        }
+    }
+}
+
+
+

d1101ebe0d546bafccca1ba9e3dc57d5045cfd33
hwangyouna03 <hwangyouna03@daum.net>
2021-06-10 21:50:54 +0900

    add information files

---
+++ app/src/main/java/kr/hs/emirim/s2019s40/pockethome1/Information_contractActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s40.pockethome1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Information_contractActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_contract);
+    }
+}
